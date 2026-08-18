499ab606321ebe5005b17225d124a0ce67bb0c6b
RecendezZ2 <zanettarecendez@gmail.com>
2026-08-17 21:00:02 -0700

    corrected spelling in JS

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ Currently, you are the creator of a life-changing app that ${dataObject.problem}
 
 You are the only person who has learned ${dataObject.language}, and they need your help designing a ${dataObject.favoriteColor}-themed system for their company. You talk things over during coffee, and they agree to pay you ${dataObject.number} times the normal pay rate.
 
-Because you have one-of-a-kind skills, you respond, "Eh, im working on a app that will fix ${dataObject.sideProject} i dont need your money, hmm why not but only if you pay me ${dataObject.two}x the normal pay rate." They agree. You get into your flying car and drive yourself to your cowarkers house and finnnish your app that  ${dataObject.sideProject} this app is a game changer..Finally your home, you pat yourself on the back and think, "you wished you stopped by 711 for some of their chicken wings." the end!!!!`
+Because you have one-of-a-kind skills, you respond, "Eh, im working on a app that will fix ${dataObject.sideProject} I don't need your money, hmm why not but only if you pay me ${dataObject.two}x the normal pay rate." They agree. You get into your flying car and drive yourself to your coworkers house and finnish your app that  ${dataObject.sideProject} this app is a game changer..Finally your home, you pat yourself on the back and think, "you wished you stopped by 711 for some of their chicken wings." the end!!!!`
 
     
     
