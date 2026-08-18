ad12bec13fb6f0ceef8ebd37597669f7b96c00bb
RecendezZ2 <zanettarecendez@gmail.com>
2026-08-18 10:43:42 -0700

    Added comments to HTML and JS

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ function onFormSubmit(event) {
 	const data = new FormData(event.target);
 	
     const dataObject = Object.fromEntries(data.entries()); 
-
-document.getElementById("story").style.color = "#ff0000"
+// we are asking to get the element with the id of story then manipulate the document by changing the color red to the story content.
+	document.getElementById("story").style.color = "#ff0000"
+	
 story.textContent = `It's the year 30${dataObject.number}. You have been working on a top-secret program that will change the world. Three men in black suits walk in and tell you to come with them. Naturally, you get out of your seat and ${dataObject.verb} in the opposite direction through a futuristic city filled with flying cars, robot assistants, and glowing buildings.
 
 Currently, you are the creator of a life-changing app that ${dataObject.problem}, but there is a ${dataObject.bug} bug that only you can fix. You look back and see that the men in suits are holding computers in their hands and yelling about a ${dataObject.error}. You decide to turn around and fix the problem. Afterward, they offer you a job at ${dataObject.dream}.
 
 You are the only person who has learned ${dataObject.language}, and they need your help designing a ${dataObject.favoriteColor}-themed system for their company. You talk things over during coffee, and they agree to pay you ${dataObject.number} times the normal pay rate.
 
-Because you have one-of-a-kind skills, you respond, "Eh, im working on a app that will fix ${dataObject.sideProject} I don't need your money, hmm why not but only if you pay me ${dataObject.two}x the normal pay rate." They agree. You get into your flying car and drive yourself to your coworkers house and finnish your app that  ${dataObject.sideProject} this app is a game changer..Finally your home, you pat yourself on the back and think, "you wished you stopped by 711 for some of their chicken wings." the end!!!!`
+Because you have one-of-a-kind skills, you respond, "Eh, im working on a app that will fix ${dataObject.sideProject} I don't need your money, hmm why not but only if you pay me ${dataObject.two}x the normal pay rate." They agree. You get into your flying car and drive yourself to your coworkers house and finnish your app that  ${dataObject.sideProject} this app is a game changer..Finally your home, you pat yourself on the back and think, "you wished you stopped by 711 for some of their chicken wings." the end!!!`
 
     
     
